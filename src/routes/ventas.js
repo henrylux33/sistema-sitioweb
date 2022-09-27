491a0f754ad9145cb75f9acb05f5ac3c5155c6fa
Danny <15daixca@gmail.com>
2022-09-26 21:38:25 -0600

    Agregue ruta a app.js, agregue VENTAS a views, Agregue ventas.js a routes

---
+++ src/routes/ventas.js
@@ -0,0 +1,9 @@
+const { Router }  = require('express');
+const router = Router();
+
+router.get('/ventas/agregar', (req, res) => {
+    res.render('VENTAS/ventas-p');
+    
+});
+
+module.exports = router;
