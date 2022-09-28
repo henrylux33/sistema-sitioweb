ea27e94b5336040f861b2bcd3c094ae8b7133196
Danny <15daixca@gmail.com>
2022-09-28 17:02:44 -0600

    Modifique y agregue una archivo a la carpeta VENTAS

--- src/routes/ventas.js
+++ src/routes/ventas.js
@@ -2,16 +2,16 @@ const { Router }  = require('express');
 const Ventas = require('../models/ventas')
 const router = Router();
 
-router.get('/ventas/agregar1', (req, res) => {
-    res.render('VENTAS/ventas-p');
+router.get('/ventas/ingresar', (req, res) => {
+    res.render('VENTAS/ventas-formulario');
     
 });
 
 router.post('/ventas/agregar', async (req, res) => {
     const ventas = Ventas(req.body);
     await ventas.save();
-    res.redirect('/ventas/agregar1');
-})
+    res.redirect('/ventas/ingresar');
+});
 
 router.get('/ventas/ver-ventas', async (req, res) => {
     const ver = await Ventas.find().lean();
