3a3c6261fe60108f21f7d0acb310e99b1f755304
Danny <15daixca@gmail.com>
2022-09-29 04:44:45 -0600

    Nueva declaracion en la base de datos en archivo database

--- src/database.js
+++ src/database.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true
-})
-    .then(db => console.log('BD concectado'))
-    .catch(err => console.error(err));
+// mongoose.connect(process.env.MONGODB_URI, {
+//     useNewUrlParser: true
+// })
+//     .then(db => console.log('BD concectado'))
+//     .catch(err => console.error(err));
+
+(async () => {
+    try{
+        const db = await mongoose.connect(process.env.MONGODB_URI);
+        console.log('BD concectado')
+    } catch (error) {
+        onsole.error(err)
+    }
+})();
 
